349a8d597ba8440d07942a9ec1f8e992d133757f
Prasun  Dandapat <129579131+5Prasun@users.noreply.github.com>
2026-04-25 21:53:55 +0530

    fix: simplify CORS configuration and update start script in package.json

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,7 @@ import userRouter from "./routes/user.routes.js"
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
